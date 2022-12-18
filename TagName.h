301e10afb404c3eddbb8c4945fa18cb5e31b2b6a
Osakazyuuzi <68203194+Osakazyuuzi@users.noreply.github.com>
2022-12-18 16:30:26 +0900

    FieldをGroundに変更

--- TagName.h
+++ TagName.h
@@ -4,7 +4,7 @@ namespace TagName {
 	const std::string Untagged = "Untagged";
 	const std::string MainCamera = "MainCamera";
 	// ˆÈ‰º‚É’Ç‰Á
-	const std::string Field = "Field";
+	const std::string Field = "Ground";
 	const std::string Wall = "Wall";
 	const std::string Player = "Player";
 	const std::string Enemy = "Enemy";
