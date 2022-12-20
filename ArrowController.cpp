39b2005a50b4101f3f776e3bb962ff9c9f2807a5
Osakazyuuzi <68203194+Osakazyuuzi@users.noreply.github.com>
2022-12-20 10:49:14 +0900

    矢の種類によっての当たり判定の処理の変更

--- ArrowController.cpp
+++ ArrowController.cpp
@@ -13,17 +13,13 @@ void ArrowController::Start()
 
 void ArrowController::Update()
 {
-	//--- 矢の進行方向に向きを合わせるプログラム
-	// 現在の正面のベクトル
-	GetOwner()->GetComponent<Transform>()->GetVectorForword();
-	// モデルの正面方向のベクトル
-	DirectX::XMFLOAT3 zVector = { 0.0f, 0.0f, -1.0f };
 }
 
 void ArrowController::OnCollisionEnter(ObjectBase* object)
 {
 	if (object->GetTag() == TagName::Enemy) {
-		ObjectManager::RemoveObject(GetOwner()->GetThisPtr());
+		if (m_ArrowType == ARROW_TYPE::NORMAL)
+			ObjectManager::RemoveObject(GetOwner()->GetThisPtr());
 	}
 }
 
