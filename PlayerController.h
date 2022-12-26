2b4d6fb5785c8934fdc806b9e44c4c7c18bcfe21
Osakazyuuzi <68203194+Osakazyuuzi@users.noreply.github.com>
2022-12-26 18:58:05 +0900

    残機の数を5に変更

--- PlayerController.h
+++ PlayerController.h
@@ -33,7 +33,7 @@ public:
 	PlayerController()
 		: m_ChargeTime(30.0f) // 60 fps の場合 0.5 秒
 		, m_Life(4)
-		, m_Zanki(9)
+		, m_Zanki()
 		, m_bLifeFlg(false)
 		, m_FlgCount(5.0f)
 		, m_bKnockBackFlg(false)
