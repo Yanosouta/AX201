27baf602d4a8af8df0d94e72269d0cb25cc3b573
WNHS10059\Sohta <souta.0432@gmail.com>
2023-01-06 21:40:11 +0900

    ボスを追加、エネミーの動きを修正

--- EnemyManager.h
+++ EnemyManager.h
@@ -18,11 +18,14 @@ public:
 
 	//敵の最大数
 	constexpr static int mc_EnemyMaxNum = 10;
+	
 private:
 	//リポップする間隔
 	float m_RepopTime;
 	//フレームカウント
 	float m_tick;
+	//ラスボスが出るまでの敵の撃退数
+	int m_AppearCount;
 	std::vector<DirectX::XMFLOAT3> m_RepopPosList;
 };
 
