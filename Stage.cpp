184ff695a54eaf6f72a4a503d28445e077887698
Osakazyuuzi <shiraisouma1212@icloud.com>
2023-01-14 22:29:14 +0900

    ステージの壁の当たり判定を追加

--- Stage.cpp
+++ Stage.cpp
@@ -27,12 +27,75 @@ Stage::Stage()
 	//--- è∞
 	// ñºëO : Ground
 	// É^ÉO : Ground
-	pObj = ObjectManager::CreateObject<Cube>("Ground", TagName::Ground);
+	pObj = ObjectManager::CreateObject<ObjectBase>("Ground", TagName::Ground);
 	pCol = pObj->AddComponent<AABBCollider>();
 	pTrans = pObj->GetComponent<Transform>();
 	pTrans->SetPosition({ -8.0f, -6.4f, -0.0f });
 	pTrans->SetScale({ 88.0f,1.0f,41.5f });
 	pCol->SetLen({ 88.0f,1.0f,41.5f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ -35.6f, 0.0f, -18.0f });
+	pTrans->SetScale({ 2.4f, 10.0f, 10.0f });
+	pCol->SetLen({ 2.4f, 10.0f, 10.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ -35.6f, 0.0f, 0.0f });
+	pTrans->SetScale({ 2.4f, 10.0f, 10.0f });
+	pCol->SetLen({ 2.4f, 10.0f, 10.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ 25.2f, 0.0f, 0.0f });
+	pTrans->SetScale({ 1.4f, 10.0f, 10.0f });
+	pCol->SetLen({ 1.4f, 10.0f, 10.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ 25.2f, 0.0f, -18.0f });
+	pTrans->SetScale({ 1.4f, 10.0f, 10.0f });
+	pCol->SetLen({ 1.4f, 10.0f, 10.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ 25.2f, 0.0f, 4.5f });
+	pTrans->SetScale({ 50.0f, 10.0f, 1.0f });
+	pCol->SetLen({ 50.0f, 10.0f, 1.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ -33.2f, 0.0f, 4.5f });
+	pTrans->SetScale({ 50.0f, 10.0f, 1.0f });
+	pCol->SetLen({ 50.0f, 10.0f, 1.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ -5.0f, 0.0f, 21.0f });
+	pTrans->SetScale({ 10.0f, 10.0f, 1.0f });
+	pCol->SetLen({ 10.0f, 10.0f, 1.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ 0.0f, 0.0f, 16.0f });
+	pTrans->SetScale({ 1.0f, 10.0f, 25.0f });
+	pCol->SetLen({ 1.0f, 10.0f, 25.0f });
+
+	pObj = ObjectManager::CreateObject<ObjectBase>("Wall", TagName::Wall);
+	pCol = pObj->AddComponent<AABBCollider>();
+	pTrans = pObj->GetComponent<Transform>();
+	pTrans->SetPosition({ -8.0f, 0.0f, 16.0f });
+	pTrans->SetScale({ 1.0f, 10.0f, 25.0f });
+	pCol->SetLen({ 1.0f, 10.0f, 25.0f });
 }
 
 Stage::~Stage()
