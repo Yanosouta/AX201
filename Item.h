c77de878c796c0f569e8a67a22ada7b349c5b690
Osakazyuuzi <shiraisouma1212@icloud.com>
2023-01-14 22:29:36 +0900

    アイテムコントローラーをアイテムに追加

--- Item.h
+++ Item.h
@@ -2,6 +2,7 @@
 
 #include "Object3D.h"
 #include "AABBCollider.h"
+#include "ItemController.h"
 
 class Item : public Object3D
 {
@@ -9,9 +10,10 @@ public:
 	// コンストラクタ
 	Item(std::string name, std::string tag) : Object3D(name, tag) {
 		// プレイヤーのモデルをロードする（仮でキューブのオブジェクト）
-		GetComponent<MeshRenderer>()->LoadModel("Assets/Model/DemoCube/DemoCube.fbx", 1.0f);
+		GetComponent<MeshRenderer>()->LoadModel("Assets/Model/DemoCube/DemoCube.fbx", 0.5f);
 		// コンポーネントを追加
 		AddComponent<AABBCollider>();
+		AddComponent<ItemController>();
 		// 当たり判定の辺の長さをセット
 		GetComponent<AABBCollider>()->SetLen({ 0.5f,0.5f,0.5f });
 	}
